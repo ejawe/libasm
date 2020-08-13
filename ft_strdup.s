e0663ec5d76441a74d3cef53120160ccca9e6fb7
ejawe <doriane.montgenie@gmail.com>
2020-08-13 18:55:10 +0200

    libasm

--- ft_strdup.s
+++ ft_strdup.s
@@ -6,16 +6,16 @@ section .text
 
 ft_strdup:	
 
-	call ft_strlen ;s'exerce sur rdi
-	inc rax ;pour rajouter le \0
-	push rdi 		;saving rdi before calling a function
-	mov	rdi, rax 		;rdi = rax; (because rdi will be used by malloc), on alloue nb de bytes
-	call malloc ;malloc len bytes, renvoie une adresse, dans rax
-	test rax, rax ;je test rax, si jz 
+	call ft_strlen				; s'exerce sur rdi
+	inc rax						; "rax" : stock le retour d'une fonction. inc rax : pour rajouter le \0
+	push rdi 					; "push" : permet d'empiler un mot dans la pile. On sauvegarde rdi avant l'appel d'une fonction
+	mov	rdi, rax 				; rdi = rax; (car rdi sera utilise par malloc), on alloue nb de bytes
+	call malloc					; malloc nb bytes, renvoie une adresse, dans rax
+	test rax, rax 				; je test rax, si jz 
 	jz return
-	pop rdi ;return mon rdi (ma src) ;je peux pop dans rsi directement
-	mov rsi, rdi ;je mets ma src dans rsi, deuxieme arg qui sera appele par strcpy
-	mov rdi, rax ;je mets rax, mon adresse, dans rdi pour utilisation dans strcpy come dst, rsi etant ma src
+	pop rdi 					; return mon rdi (ma src) ;je peux pop dans rsi directement
+	mov rsi, rdi				; je mets ma src dans rsi, deuxieme arg qui sera appele par strcpy
+	mov rdi, rax				; je mets rax, mon adresse, dans rdi pour utilisation dans strcpy come dst, rsi etant ma src
 	call ft_strcpy
 
 return:
